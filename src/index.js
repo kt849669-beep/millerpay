6bec6dd37c2c0e290ae46ea1dda037f65b7d4621
kt849669-beep <kt849669@gmail.com>
2026-09-03 11:05:20 +0530

    Load backend through Vercel Express handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
-import app from '../server/src/index.js'
 
-void express
+const app = express()
+let application
+
+app.use(async (request, response, next) => {
+  try {
+    application ||= import('../server/src/index.js').then((module) => module.default)
+    const handler = await application
+    return handler(request, response, next)
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default app
