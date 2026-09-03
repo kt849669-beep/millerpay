2342ca68e9145678774a629262a77392745df3ae
kt849669-beep <kt849669@gmail.com>
2026-09-03 11:07:52 +0530

    Revert "Serve generated frontends from Vercel Express"
    This reverts commit 967f9ba54e61a1eb589c7227a9544524f472953e.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import express from 'express'
-import { fileURLToPath } from 'node:url'
 
 const app = express()
 let application
-const publicDirectory = fileURLToPath(new URL('../public/', import.meta.url))
-
-app.use(express.static(publicDirectory, { index: false }))
 
 app.use(async (request, response, next) => {
   try {
